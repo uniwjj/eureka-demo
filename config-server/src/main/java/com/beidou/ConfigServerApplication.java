b3118cb9cccdd1a9f1d5cc9601a1181b5746cd48
ginger <uniwjj@gmail.com>
2019-04-09 10:53:05 +0800

    增加配置文件

---
+++ config-server/src/main/java/com/beidou/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.beidou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置服务启动类
+ *
+ * @author wangjinjie
+ * @create 2019-04-09 9:53
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
